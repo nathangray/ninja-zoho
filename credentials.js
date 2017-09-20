6b3cc1fac06dc001b46ea41537dfb1c53e165a06
Nathan Gray <nathangray@users.noreply.github.com>
2017-09-20 13:18:09 -0600

    Fix credentials format

--- credentials.js
+++ credentials.js
@@ -1,11 +1,12 @@
-const NinjaAPI = {
-	accessKeyId: 'TF4STGMDR4H7AEXAMPLE',
+module.exports = {
+
+NinjaAPI: {
+	accessKeyID: 'TF4STGMDR4H7AEXAMPLE',
 	secret: 'eh14c4ngchhu6283he03j6o7ar2fcuca0example',
 	host: 'http://api.ninjarmm.com',
-};
+},
 
-const ZohoApi = {
+ZohoApi: {
 	Authorization: 'cb3ba8197c840d0d61f2398cdbexample',
 	host: 'https://desk.zoho.com/api/v1/'
-};
-	
+}}
